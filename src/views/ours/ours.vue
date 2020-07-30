17a3f74f96ff8f35ec288959f47a6dc5aa1e09e6
gongzhihao <18438628607@163.com>
2020-07-30 16:37:32 +0800

    我的 表头

--- src/views/ours/ours.vue
+++ src/views/ours/ours.vue
@@ -1,5 +1,8 @@
 <template>
-  <div class="ours"></div>
+  <div class="ours">
+    <div class="joinActivityHeader">我的</div>
+    <div class="oursInfo"></div>
+  </div>
 </template>
 <script>
 export default {
@@ -8,3 +11,25 @@ export default {
   }
 }
 </script>
+<style lang="scss">
+.ours {
+  .joinActivityHeader {
+    display: flex;
+    justify-content: center;
+    height: 45px;
+    line-height: 45px;
+    font-size: 16px;
+    color: #fff;
+    background: url(./../../assets/img/oursHeader.png) no-repeat;
+    background-size: 100% 100%;
+  }
+  .ours {
+    position: relative;
+    height: 100px;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    padding: 0 20px;
+  }
+}
+</style>
